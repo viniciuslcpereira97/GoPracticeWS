c2b1c23a5681d90a0df5724e174811072301ee79
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-10-27 11:43:47 -0200

    Get database config from file

--- DB/Connection.go
+++ DB/Connection.go
@@ -5,15 +5,18 @@ import (
     "practice-ws/config"
 )
 
+// Connect to Mongo Database
 func Connect() (* mgo.Database) {
 
-    db_host := config.GetConfig("db").(string)
+    db_host := config.GetConfig("db_host").(string)
+    db_name := config.GetConfig("db_name").(string)
+
     session, err := mgo.Dial(db_host)
     
     if err != nil {
         panic(err)
     }
 
-    return session.DB("practice_db")
+    return session.DB(db_name)
 
 }

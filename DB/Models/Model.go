9ca5c086bcec3b4bb61db84d60a036e62004b5cd
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-10-27 11:45:47 -0200

    Some comments added

--- DB/Models/Model.go
+++ DB/Models/Model.go
@@ -1,5 +1,6 @@
 package models
 
+// Model contract interface
 type Model interface {
     Create(model interface {})
     Find(conditions interface {}, fields interface {})
--- DB/Models/User.go
+++ DB/Models/User.go
@@ -6,16 +6,19 @@ import (
 
 var collection = db.Connect().C("users")
 
+// User struct
 type User struct {
     Name string
     Age int
     Status string
 }
 
+// Insert new user at database
 func Create(user interface {}) {
     collection.Insert(&user)
 }
 
+// Find User by conditions
 func Find(conditions interface {}, fields interface {}) (user map[string] interface {}) {
     collection.Find(conditions).Select(fields).One(&user)
     return user

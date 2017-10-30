0d4bc0b21eec19abfdb7e1e920f58c116c848556
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-10-30 16:26:00 -0200

    registering routes and server able to listen and serve

--- Routes/Router.go
+++ Routes/Router.go
@@ -2,29 +2,67 @@ package routes
 
 import (
     "fmt"
-    "html"
     "net/http"
     "github.com/gorilla/mux"
 )
 
+// Route struct
+type Route struct {
+    Name string
+    Path string
+    Methods string
+    Handler http.HandlerFunc
+}
+
+// Array of Routes
+var routes []Route
+
+// Router instance
 var Router *mux.Router
 
+// Initialize new Router instance
 func InitRouter() {
     if Router == nil {
         Router = mux.NewRouter()
     }
 }
 
-func AddRoutes(route string, methods string) {
+// Register POST route
+func Post(name string, route string, handler http.HandlerFunc) {
+    new_route := Route {
+        name,
+        route,
+        "POST",
+        handler,
+    }
+
+    routes = append(routes, new_route)
+}
+
+// Register new GET route
+func Get(name string, route string, handler http.HandlerFunc) {
+    new_route := Route {
+        name, 
+        route,
+        "GET",
+        handler,
+    }
+
+    routes = append(routes, new_route)
+}
+
+// Handle all registered routes before provide Router
+func LoadRoutes() {
     InitRouter()
-    Router.HandleFunc(route, handler).Methods(methods)
+
+    for _, route := range routes {
+        Router.HandleFunc(route.Path, route.Handler).Methods(route.Methods)
+    }
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-}
-
+// Loads all Routes
+// Start to Listen and Serve
 func Serve() {
-    AddRoutes("/teste","GET")
+    LoadRoutes()
     http.ListenAndServe(":8888", Router)
 }
--- playground.go
+++ playground.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+    "fmt"
+    "net/http"
     "practice-ws/Routes"
 )
 
 func main() {
+    routes.Get("say.hello", "/hello", say_hello)
     routes.Serve()    
+}
+
+func say_hello(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello World!")
 }

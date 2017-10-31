929be1b32a3d8bf770becfd4c8ba043eb16905b8
Vinicius Luiz <viniciuslcp97@gmail.com>
2017-10-31 16:30:46 -0200

    user model path udpate

--- DB/Models/Users/User.go
+++ DB/Models/Users/User.go
@@ -1,4 +1,4 @@
-package models
+package users
 
 import (
     "practice-ws/DB"
@@ -20,7 +20,7 @@ func Create(user interface {}) {
 }
 
 // Find User by conditions
-func Find(conditions interface {}, fields interface {}) (user User) {
+func Find(conditions interface {}, fields ...interface {}) (user User) {
     var result interface {}
     collection.Find(conditions).Select(fields).One(&result)
     mapstructure.Decode(result, &user)
@@ -32,6 +32,25 @@ func Find(conditions interface {}, fields interface {}) (user User) {
 func Where(conditions interface {}, fields interface {}) (users []User) {
     var result []interface{}
     collection.Find(conditions).Select(fields).All(&result)
+    mapstructure.Decode(result, &users)
+
+    return users
+}
+
+func ById(user_id string) (user User) {
+    var result interface {}
+    condition := map[string] string {
+        "_id": user_id,
+    }
+    collection.Find(condition).Select(nil).One(&result)
+    mapstructure.Decode(result, &user)
+
+    return user
+}
+
+func All() (users []User) {
+    var result []interface{}
+    collection.Find(nil).All(&result)
     mapstructure.Decode(result, &users)
 
     return users
